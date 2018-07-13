a1d02c637599eb772c15365869e70b0970d97702
HCui91 <cui980406@gmail.com>
2018-07-13 16:38:45 +0800

    Modify ouput PNG name
    Remove space in PNG name. Remove unused data file. Rename data file as low or high damping instead of date of experiment.

--- plot.py
+++ plot.py
@@ -1,8 +1,6 @@
 #Import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
-#Decide size of the output plot
-plt.figure(1,figsize=(10,9))
 
 ##### Reset variables #####
 #Expected natural frequency
@@ -20,35 +18,36 @@ L=18e-3         #Inductance of the inductor in Henry (H)
 C=1800e-12      #Capacitance of the capacitor in Faraday (F)
 R=820           #Resistance of the resistor in Ohm
 
-
 ##### Theoritical natural frequency #####
 omega_0=1/(np.sqrt(L*C))         # Theoretical natural ANGULAR frequency
 omega=np.sqrt(omega_0**2-(R**2/(2*L**2)))      # Theoretical natural frequency 
 plt.plot((omega,omega), (0, 4), 'k--',label="Theoretical amplitude resonance")    # Plot a vertical line at the natural frequency
 print("Theoretical amplitude resonance is ",omega,"radian per second, or",omega/(2*np.pi),"Hz")
 
-##### Import data (2 sets of data) #####
+##### Import data #####
 # CSV in form: frequency(Hz)	peak-to-peak voltage(volt)	unc voltage (volt)	y0 (volt)	y1 (volt)
+# There are 2 sets of data corresponding to high damping and low damping. 
 # where y0 and y1 are the Lissajous Figures on the oscilliscope.
-freq, volt, delta_volt, y0, y1, delta_y = np.loadtxt("121020171018.csv",delimiter="	",unpack=True) 
-freq_1, volt_1, delta_volt_1, y0_1, y1_1 = np.loadtxt("121020171122.csv",delimiter="	",unpack=True)
+freq, volt, delta_volt, y0, y1, delta_y = np.loadtxt("low_damp.csv",delimiter="	",unpack=True)   # Change file name and delimiter if required
+freq_1, volt_1, delta_volt_1, y0_1, y1_1 = np.loadtxt("high_damp.csv",delimiter="	",unpack=True)
 
 ##### Data modification #####
 freq=2*np.pi*freq     # Convert to angular frequency
 volt=volt/2           # From peak-to-peak voltage to amplitude
 phase=np.arcsin(y1/y0) # Phase calculation
-
+# Same treatment to the other dataset
 freq_1=2*np.pi*freq_1
 volt_1=volt_1/2
 phase_1=np.arcsin(y1_1/y0_1)
 
-#Phase angle error
-delta_y1y0=np.sqrt((delta_volt/y1)**2+(delta_volt/y0)**2)*(y1/y0)
-delta_phase = delta_y1y0/np.sqrt(1-(y1/y0)**2)
+##### Uncertainty propagation #####
+delta_y1y0=np.sqrt((delta_volt/y1)**2+(delta_volt/y0)**2)*(y1/y0)  #1 Uncertainty of the measurement
+delta_phase = delta_y1y0/np.sqrt(1-(y1/y0)**2)                     #2 Uncertainty of phase calculation
+# Same treatment to the other dataset
 delta_y1y0_1=np.sqrt((delta_volt_1/y1_1)**2+(delta_volt_1/y0_1)**2)*(y1_1/y0_1)
 delta_phase_1 = delta_y1y0_1/np.sqrt(1-(y1_1/y0_1)**2)
 
-#correcting phase angle
+##### Correction to phase angles #####
 for x in range(0,36):
     if freq[x] >= 28e3*2*np.pi:
         phase[x]=np.pi-phase[x]
@@ -56,32 +55,33 @@ for y in range(0,34):
     if freq_1[y] >= 28e3*2*np.pi:
         phase_1[y]=np.pi-phase_1[y]
 
-#Output to csv file
-np.savetxt("output0.csv",(freq,volt,delta_volt,phase,delta_phase), delimiter=",")
-np.savetxt("output1.csv",(freq_1,volt_1,delta_volt_1,phase_1,delta_phase_1), delimiter=",")
+##### Output to angular frequency and amplitude to csv file #####
+# If you want to investigate more about angular frequency and amplitude, uncomment it.
+#np.savetxt("output0.csv",(freq,volt,delta_volt,phase,delta_phase), delimiter=",")
+#np.savetxt("output1.csv",(freq_1,volt_1,delta_volt_1,phase_1,delta_phase_1), delimiter=",")
 
-#Amplitude graph
-plt.errorbar(freq,volt,yerr=delta_volt,fmt='o',label="Low damping")
+##### Amplitude graph #####
+#Decide size of the output plot
+plt.figure(1,figsize=(10,9))
+plt.errorbar(freq,volt,yerr=delta_volt,fmt='o',label="Low damping")         # Plot with errorbar
 plt.errorbar(freq_1,volt_1,yerr=delta_volt_1,fmt='D',label="High damping")
-
-#Graoh configuration
+#Graph configuration
 plt.title("Plot of Voltage resonance data")
 plt.xlabel("Angular frequence(radian per second)")
 plt.ylabel("Amplitude of Voltage(volt)")
 plt.legend(loc="best")
-plt.xlim(6e4-1000,26e4+1000)
+plt.xlim(6e4-1000,26e4+1000)          # Modify it with your frequency range
 plt.ylim(0.5,4)
 plt.grid()
-#plt.show()
-#Save graph
-plt.savefig("E7 amplitude")
+#plt.show()                           # Uncomment it if you are using command line tools
+#Save graph  
+plt.savefig("E7_amplitude")
 
-#Phase angle graph
-plt.figure(2,figsize=(10,7))
-plt.errorbar(freq,phase,yerr=delta_phase,fmt='o',label="Low damping")
+##### Phase angle graph #####
+plt.figure(2,figsize=(10,7))     # Generate another canvas
+plt.errorbar(freq,phase,yerr=delta_phase,fmt='o',label="Low damping")       # Plot with errorbar
 plt.errorbar(freq_1,phase_1,yerr=delta_phase_1,fmt='D',label="High damping")
-#plt.plot((omega,omega), (0, 4), 'k--',label="Theoretical amplitude resonance")
-plt.plot((50000,280000),(np.pi/2,np.pi/2),'--',label="Phase angle=pi/2")
+plt.plot((50000,280000),(np.pi/2,np.pi/2),'--',label="Phase angle=pi/2")    # A reference line at the phase angle where should have resonance
 plt.title("Plot of phase resonance data")
 plt.xlabel("Angular frequence(radian per second)")
 plt.ylabel("Phase angle(radian)")
@@ -90,5 +90,5 @@ plt.ylim(0,np.pi)
 plt.xlim(np.min(freq)-1000,np.max(freq)+1000)
 plt.grid()
 #plt.show()
-plt.savefig("E7 phase")
+plt.savefig("E7_phase")
 
